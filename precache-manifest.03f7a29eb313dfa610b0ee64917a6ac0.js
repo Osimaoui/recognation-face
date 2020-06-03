af16957d84c75c54ef4bdedfd45fe2ed56254115
Osimaoui <otmane.assaka@gmail.com>
2020-06-03 20:21:28 +0100

    Updates

--- precache-manifest.03f7a29eb313dfa610b0ee64917a6ac0.js
+++ precache-manifest.03f7a29eb313dfa610b0ee64917a6ac0.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "f283dbccafa8dd7c575337b59ea88967",
+    "revision": "1e6a80550b88bbe89625a83ad2c683c1",
     "url": "/recognation-face/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/recognation-face/static/css/2.d811125f.chunk.css"
   },
   {
-    "revision": "83ef58156f8611870de4",
+    "revision": "c20be79e76a194c6264a",
     "url": "/recognation-face/static/css/main.9e4072b9.chunk.css"
   },
   {
@@ -20,8 +20,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/recognation-face/static/js/2.906ce053.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "83ef58156f8611870de4",
-    "url": "/recognation-face/static/js/main.149d52ae.chunk.js"
+    "revision": "c20be79e76a194c6264a",
+    "url": "/recognation-face/static/js/main.7fef8f8f.chunk.js"
   },
   {
     "revision": "a8373e3a8b7f3de1b435",
